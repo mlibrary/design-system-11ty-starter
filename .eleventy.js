9ed28752d5f873b56b057544239ed9c2ba803639
“Bridget <burkebri@umich.edu>
2022-08-30 16:48:20 -0400

    Eleventy config for watch and passthrough CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
 module.exports = function(eleventyConfig) {
+
+  // The addWatchTarget config method allows you to manually add a file for Eleventy to watch.
+  eleventyConfig.addWatchTarget("./src/sass");
+
+
+  // The Pass Through feature tells Eleventy to copy things to our output folder
+  // Eleventy passes through our compiled CSS to the public directory. 
+  eleventyConfig.addPassthroughCopy("./src/css");
+
   return {
     passthroughFileCopy: true,
     dir: {
